137a25ab17e44eabef32ded0688af961372bdb9e
Nikola Stevanovic <nikola.stevanovic@sbgenomics.com>
2022-08-19 17:41:46 +0200

    Add schemas and update test routes

--- routers/recipe.py
+++ routers/recipe.py
@@ -7,21 +7,20 @@ from dependencies import get_db
 from models.ingredient import Ingredient
 from models.recipe import Recipe
 
+from schemas.recipe import Recipe as RecipeSchema
+from schemas.recipe import RecipeCreate
+from schemas.ingredient import Ingredient as IngredientSchema
+from schemas.ingredient import IngredientCreate
+
 recipe_router = APIRouter()
 
 
-@recipe_router.post('/v1', response_model=Any)
+@recipe_router.post('/v1', response_model=RecipeSchema)
 def create_new_recipe(
-        recipe: dict = Body(...), db: Session = Depends(get_db)
+        recipe: RecipeCreate, db: Session = Depends(get_db)
 ) -> Any:
-    """
-    Create new recipe.
-    :param recipe: Recipe model
-    :param db: Current database session
-    :returns: Newly created recipe
-    """
     ingredient_list = []
-    for ingredient_name in recipe['ingredients']:
+    for ingredient_name in recipe.ingredients:
         ingredient = db.query(Ingredient).filter(
             Ingredient.name == ingredient_name
         ).first()
@@ -30,24 +29,24 @@ def create_new_recipe(
             ingredient_list.append(ingredient)
 
     new_recipe = Recipe(
-        name=recipe['name'],
-        preparation=recipe['preparation'],
+        name=recipe.name,
+        preparation=recipe.preparation,
         ingredients=ingredient_list
     )
 
     db.add(new_recipe)
     db.commit()
 
-    return recipe
+    return new_recipe
 
 
-@recipe_router.post('/v2')
+@recipe_router.post('/v2', response_model=IngredientSchema)
 def create_new_ingredient(
-        ingredient: dict = Body(...), db: Session = Depends(get_db)
+        ingredient: IngredientCreate, db: Session = Depends(get_db)
 ) -> Any:
-    new_ingredient = Ingredient(name=ingredient['name'])
+    new_ingredient = Ingredient(name=ingredient.name)
 
     db.add(new_ingredient)
     db.commit()
 
-    return ingredient
+    return new_ingredient
---
+++ schemas/recipe.py
@@ -0,0 +1,28 @@
+from uuid import UUID
+from datetime import datetime
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class RecipeBase(BaseModel):
+    class Config:
+        orm_mode = True
+
+
+class RecipeCreate(RecipeBase):
+    name: str
+    preparation: str
+    ingredients: List[str]
+
+
+class Recipe(RecipeBase):
+    id: UUID
+    name: str
+    preparation: str
+    ingredients: 'List[Ingredient]'
+    created_at: datetime
+    modified_at: Optional[datetime]
+
+
+from .ingredient import Ingredient # noqa
+Recipe.update_forward_refs()
